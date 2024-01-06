ed95f95646280233dd16089336a55e6ad36fa60d
Amit-Yadav21 <amit21@navgurukul.org>
2024-01-06 23:21:21 +0530

    add more questions

--- code_snippet.js
+++ code_snippet.js
@@ -527,3 +527,14 @@ const example = ({ a, b, c }) => {
 // Now, when JavaScript tries to compare true === 3, it attempts to compare a boolean (true) with a number (3). The === operator checks for both value and type equality.
 // This comparison will result in false because true (boolean) is not strictly equal to 3 (number).
 // Therefore, the output of the code console.log(3 === 3 === 3); will be false.
+
+// 0 == false   // true
+// 0 === false  // false
+// 1 == "1"     // true
+// 1 === "1"    // false
+// null == undefined // true
+// null === undefined // false
+// '0' == false // true
+// '0' === false // false
+// []==[] or []===[] //false, refer different objects in memory
+// {}=={} or {}==={} //false, refer different objects in memory
