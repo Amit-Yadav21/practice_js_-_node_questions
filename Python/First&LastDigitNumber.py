b1ec35c635090c0f25a5be67807979a229f2e1b0
Amit-Yadav21 <amit21@navgurukul.org>
2024-01-18 01:44:08 +0530

    Add file

---
+++ Python/First&LastDigitNumber.py
@@ -0,0 +1,20 @@
+# number = 2000
+# last_two_digits = number % 1000
+# print(last_two_digits)
+
+# number = 2000
+# first_two_digits = str(number)[:2]
+# Last_two_digits = str(number)[-2:]
+# last_two_digits = int(str(number)[-2:])
+# last_Two_digits = int(str(number)[:2])
+# print(first_two_digits,Last_two_digits,last_two_digits,last_Two_digits)
+
+# import math
+# number = 202411
+# last_two_digits = int(math.fmod(number, 100))
+# print(last_two_digits)
+
+# import math
+# number = 12345
+# first_two_digits = int(math.floor(number / 1000))
+# print(first_two_digits)
