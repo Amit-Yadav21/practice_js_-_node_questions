734400d060d99ffe24e7df61a37966ffdb95de68
Amit-Yadav21 <amit21@navgurukul.org>
2024-01-18 09:14:52 +0530

    Add one more file in js

---
+++ LogicalQuestionJavaScript/First&LastNumber.js
@@ -0,0 +1,12 @@
+// let number = 12345;
+// let numberString = number.toString();
+// // Find first two digits
+// let firstTwoDigits = parseInt(numberString.slice(0, 2));
+// // Find last two digits
+// let lastTwoDigits = parseInt(numberString.slice(-2));
+// console.log(numberString,firstTwoDigits,lastTwoDigits);
+
+// let number = 12345;
+// let firstTwoDigits = Math.floor(number / 1000);   // Find first two digits
+// let lastTwoDigits = number % 100;   // Find last two digits
+// console.log(firstTwoDigits, lastTwoDigits);
